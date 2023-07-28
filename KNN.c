1a48bdf550caf62419be1d46b0452c518575c60f
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-07-28 11:02:22 -0300

    ex

--- KNN.c
+++ KNN.c
@@ -4,27 +4,28 @@
 #include <math.h>
 
 int main(){
-  float a, b, c, d, distM, w, x, y, z, dist;
+  float a, b, c, d, k, w, x, y, z, dist; 
   int classeR, classe, classeP, acerto = 0;
 
+  /*Abrindo arquivos*/
   FILE *arquivoTeste = fopen("IrisTeste.txt","r");
   FILE *arquivoTreino = fopen("IrisTreino.txt","r");
   while(!feof(arquivoTeste)){
-    distM = 1;
-  fscanf(arquivoTeste,"%f,%f,%f,%f,%d",&a,&b,&c,&d,&classeR);
-  //printf("%f,%f,%f,%f,%d",a,b,c,d,classeR);
-  arquivoTreino = fopen("IrisTreino.txt","r");
+    k = 1; // instanciando k sendo igual a 1
+  fscanf(arquivoTeste,"%f,%f,%f,%f,%d",&a,&b,&c,&d,&classeR); // leitura de arquivo
+  arquivoTreino = fopen("IrisTreino.txt","r"); // abrindo arquivo
   while(!feof(arquivoTreino)){
-    fscanf(arquivoTreino,"%f,%f,%f,%f,%d",&w,&x,&y,&z,&classe);
-    //printf("%f,%f,%f,%f,%d",w,x,y,z,classe);
-    dist = sqrt(pow(w-a, 2)+ pow(x-b, 2) + pow(y-c, 2) + pow(z-d,2));
-    if(dist < distM){
-      distM = dist;
+    fscanf(arquivoTreino,"%f,%f,%f,%f,%d",&w,&x,&y,&z,&classe); // leitura de arquivo
+    dist = sqrt(pow(w-a, 2)+ pow(x-b, 2) + pow(y-c, 2) + pow(z-d,2)); // calculando a distancia
+    /*Com o if se instancia as classes*/
+    if(dist < k){
+      k = dist;
       classeP = classe;
     }
   }
+    /*Conta quantidade de acertos*/
      if(classeP == classeR){
-    acerto++;
+     acerto++;
   fclose(arquivoTreino);
   }
    }
